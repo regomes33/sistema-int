f07e027a9e3bc02cb1fb6e9e07caea9c6d63dfb1
regomes33 <regomes33@gmail.com>
2019-12-30 14:06:10 -0300

    atualizando

--- ari/settings.py
+++ ari/settings.py
@@ -124,12 +124,6 @@ LOGOUT_REDIRECT_URL = 'login'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '{}/media'.format(BASE_DIR)
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
