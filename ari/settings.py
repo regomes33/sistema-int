a641ba21a7cc90bf2a784ffccafffae5cdd3cf5e
regomes33 <regomes33@gmail.com>
2020-01-02 16:02:08 -0300

    melhor

--- ari/settings.py
+++ ari/settings.py
@@ -125,5 +125,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = '{}/media'.format(BASE_DIR)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
