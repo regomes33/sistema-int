8b7c29b1bcd000833e8b3ed188cc659ab445ffae
rg3915 <rg3915@yahoo.com.br>
2020-02-25 19:55:22 -0300

    Pessoas: salvar tatuagens. Close #31

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ import json
 from pprint import pprint
 from django.http import JsonResponse
 from localflavor.br.br_states import STATE_CHOICES
-from pessoa.models import Pessoa, Faccao, Foto
+from pessoa.models import Pessoa, Faccao, Foto, Tatuagem
 from pessoa.forms import PessoaForm, PessoaContatoForm, PessoaVeiculoForm
 from ocorrencia.models import Natureza, Arma, Ocorrencia
 from ocorrencia.forms import InfracaoForm, PessoaOcorrenciaForm
@@ -30,9 +30,22 @@ def pessoa_add(request):
         data = form.data
         data['pk'] = pessoa_post.pk
 
+        files = request.FILES.items()
+        photos, tattoos = [], []
+
+        for name, file in files:
+            if 'photo' in name:
+                photos.append(file)
+            if 'tattoo' in name:
+                tattoos.append(file)
+
         # Adiciona Fotos
-        for photo in request.FILES.values():
+        for photo in photos:
             Foto.objects.create(pessoa=pessoa_post, foto=photo)
+
+        # Adiciona Tatuagens
+        for tattoo in tattoos:
+            Tatuagem.objects.create(pessoa=pessoa_post, foto=tattoo)
 
         # Adiciona Infrações
         infracoes_data = json.loads(request.POST.get('infracoes'))
