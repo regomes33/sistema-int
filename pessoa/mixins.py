8e26c097471651b600fc0407c03e4ece51c7c0d7
rg3915 <rg3915@yahoo.com.br>
2020-03-19 16:27:20 -0300

    Pessoas: filtro por infração no front. Close #66

--- pessoa/mixins.py
+++ pessoa/mixins.py
@@ -6,10 +6,27 @@ class SearchMixin(object):
     def get_queryset(self):
         queryset = super(SearchMixin, self).get_queryset()
         search = self.request.GET.get('search')
+        filter_natureza = self.request.GET.get('filter_natureza')
+        filter_bairro = self.request.GET.get('filter_bairro')
+        filter_faccao = self.request.GET.get('filter_faccao')
+        filter_cidade = self.request.GET.get('filter_cidade')
         if search:
-            return queryset.filter(
+            queryset = queryset.filter(
                 Q(nome__icontains=search) |
                 Q(sobrenome__icontains=search) |
                 Q(apelido__icontains=search)
             )
+
+        if filter_natureza:
+            queryset = queryset.filter(Q(infracao__natureza=filter_natureza))
+
+        if filter_bairro:
+            queryset = queryset.filter(Q(district=filter_bairro))
+
+        if filter_cidade:
+            queryset = queryset.filter(Q(city=filter_cidade))
+
+        if filter_faccao:
+            queryset = queryset.filter(Q(faccao=filter_faccao))
+
         return queryset
--- pessoa/views.py
+++ pessoa/views.py
@@ -7,9 +7,9 @@ from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import ListView
-from .models import Pessoa
+from .models import Pessoa, Faccao
 from .mixins import SearchMixin
-from ocorrencia.models import PessoaOcorrencia
+from ocorrencia.models import PessoaOcorrencia, Natureza
 
 
 class PessoasList(LRM, SearchMixin, ListView):
@@ -22,6 +22,36 @@ class PessoasList(LRM, SearchMixin, ListView):
         context['model_name_plural'] = 'Pessoas'
         context['pessoas_total'] = Pessoa.objects.values_list(
             'id', flat=True).count()
+
+        context['naturezas'] = Natureza.objects.values_list('pk', 'natureza')
+
+        bairros = Pessoa.objects.values_list(
+            'district', flat=True)
+        context['bairros'] = sorted(
+            set([bairro for bairro in bairros if bairro]))
+
+        cidades = Pessoa.objects.values_list(
+            'city', flat=True)
+        context['cidades'] = sorted(
+            set([cidade for cidade in cidades if cidade]))
+
+        context['faccoes'] = Faccao.objects.values_list('pk', 'nome')
+
+        # Devolve o valor selecionado pra manter o filtro aplicado no template.
+        filter_natureza = self.request.GET.get('filter_natureza')
+        filter_bairro = self.request.GET.get('filter_bairro')
+        filter_faccao = self.request.GET.get('filter_faccao')
+        filter_cidade = self.request.GET.get('filter_cidade')
+
+        if filter_natureza:
+            context['selected_natureza'] = str(filter_natureza)
+        if filter_bairro:
+            context['selected_bairro'] = str(filter_bairro)
+        if filter_faccao:
+            context['selected_faccao'] = str(filter_faccao)
+        if filter_cidade:
+            context['selected_cidade'] = str(filter_cidade)
+
         return context
 
 
