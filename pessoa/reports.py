98f1fc655d6be76eae851a0bb6cc1fcb52ae668c
rg3915 <rg3915@yahoo.com.br>
2020-04-06 21:58:32 -0300

    Imprimir relatório de Pessoas: colocar RG e CPF. Close #87

--- pessoa/reports.py
+++ pessoa/reports.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
 from .models import Pessoa
 
 
 def report_pessoas(request):
     pessoas = Pessoa.objects.all()
-    context = {'object_list': pessoas}
+    context = {
+        'object_list': pessoas,
+        'today': datetime.now().today()
+    }
     template_name = 'relatorios/report_pessoas.html'
     return render(request, template_name, context)
