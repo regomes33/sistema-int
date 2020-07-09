06c5b5ea44c7462b15f117da9d51147258452d0d
rg3915 <rg3915@yahoo.com.br>
2020-07-08 22:50:29 -0300

    Recuperar filename_auth_user. fix #117

--- utils/import_data.py
+++ utils/import_data.py
@@ -27,68 +27,84 @@ from veiculo.models import Veiculo
 
 path = 'https://res.cloudinary.com/sistema-int/raw/upload'
 
-filename_user = f'{path}/v1588391446/csv/auth_user_c3md4d.csv'
-filename_pessoa_faccao = f'{path}/v1588391160/csv/pessoa_faccao_lb17pd.csv'
-filename_pessoa_pessoa = f'{path}/v1588386442/csv/pessoa_pessoa_ei4ado.csv'
-filename_pessoa_foto = f'{path}/v1588386919/csv/pessoa_foto_vakddv.csv'
-filename_pessoa_comparsa = f'{path}/v1588463742/csv/pessoa_comparsa_q52hvu.csv'
-filename_pessoa_ocorrencia = f'{path}/v1588536338/csv/ocorrencia_pessoaocorrencia_omw4h7.csv'
-filename_natureza = f'{path}/v1588394597/csv/ocorrencia_natureza_z6ytfb.csv'
-filename_arma = f'{path}/v1588394764/csv/ocorrencia_arma_bmfnm9.csv'
-filename_ocorrencia = f'{path}/v1588394853/csv/ocorrencia_ocorrencia_wgacov.csv'
-filename_areaupm = f'{path}/v1588395968/csv/ocorrencia_areaupm_qk84df.csv'
-filename_motivacao = f'{path}/v1588396087/csv/ocorrencia_motivacao_zp5cpb.csv'
-filename_infracao = f'{path}/v1588466176/csv/ocorrencia_infracao_nxg1sl.csv'
-filename_veiculo_cor = f'{path}/v1588385001/csv/veiculo_cor_iq3e7i.csv'
-filename_veiculo_modelo = f'{path}/v1588385550/csv/veiculo_modelo_ury3nj.csv'
-filename_veiculo_veiculo = f'{path}/v1588386054/csv/veiculo_veiculo_bjwhpq.csv'
+filename_auth_user = f'{path}/v1594243966/csv/auth_user_200708_z7wcnb.csv'
+filename_ocorrencia_areaupm = f'{path}/v1594243966/csv/ocorrencia_areaupm_200708_fji0wr.csv'
+filename_ocorrencia_arma = f'{path}/v1594243966/csv/ocorrencia_arma_200708_gxnpjn.csv'
+filename_ocorrencia_autoria = f'{path}/v1594243966/csv/ocorrencia_autoria_200708_wcoufy.csv'
+filename_ocorrencia_genero = f'{path}/v1594243967/csv/ocorrencia_genero_200708_ns2sp1.csv'
+filename_ocorrencia_homicidio = f'{path}/v1594243967/csv/ocorrencia_homicidio_200708_x7qqeo.csv'
+filename_ocorrencia_infracao = f'{path}/v1594243967/csv/ocorrencia_infracao_200708_jdo8yj.csv'
+filename_ocorrencia_motivacao = f'{path}/v1594243967/csv/ocorrencia_motivacao_200708_ek0ylz.csv'
+filename_ocorrencia_natureza = f'{path}/v1594243967/csv/ocorrencia_natureza_200708_tvpwhg.csv'
+filename_ocorrencia_ocorrencia = f'{path}/v1594243967/csv/ocorrencia_ocorrencia_200708_isamya.csv'
+filename_ocorrencia_ocorrenciaveiculo = f'{path}/v1594243967/csv/ocorrencia_ocorrenciaveiculo_200708_cp53uj.csv'
+filename_ocorrencia_pessoaocorrencia = f'{path}/v1594243967/csv/ocorrencia_pessoaocorrencia_200708_rq29s4.csv'
+filename_pessoa_comparsa = f'{path}/v1594243967/csv/pessoa_comparsa_200708_crlvnd.csv'
+filename_pessoa_faccao = f'{path}/v1594243967/csv/pessoa_faccao_200708_mz2xt1.csv'
+filename_pessoa_foto = f'{path}/v1594243967/csv/pessoa_foto_200708_rvrtgl.csv'
+filename_pessoa_pessoa = f'{path}/v1594243967/csv/pessoa_pessoa_200708_byrb4x.csv'
+filename_pessoa_pessoacontato = f'{path}/v1594243968/csv/pessoa_pessoacontato_200708_lfa9hw.csv'
+filename_pessoa_pessoaveiculo = f'{path}/v1594243968/csv/pessoa_pessoaveiculo_200708_c1yph9.csv'
+filename_pessoa_tatuagem = f'{path}/v1594243968/csv/pessoa_tatuagem_200708_rwg6dl.csv'
+filename_veiculo_cor = f'{path}/v1594243968/csv/veiculo_cor_200708_mi1rgw.csv'
+filename_veiculo_modelo = f'{path}/v1594243968/csv/veiculo_modelo_200708_z1fgmr.csv'
+filename_veiculo_veiculo = f'{path}/v1594243968/csv/veiculo_veiculo_200708_azri8x.csv'
 
 
-def read_data(filename):
+def read_data(items):
     '''
     Lê os dados para extrair id e slug e montar um dicionario.
     '''
-    df = pd.read_csv(filename)
-    df = df[['id', 'slug']]
     dictionary = {}
-    for row in df.itertuples():
-        dictionary[str(row.id)] = row.slug
+    for item in items:
+        dictionary[str(item['id'])] = item['slug']
     return dictionary
 
 
-dict_users = {}
+def csv_online_to_list(url: str) -> list:
+    '''
+    Lê um CSV a partir de uma url.
+    '''
+    url_open = urllib.request.urlopen(url)
+    reader = csv.DictReader(io.StringIO(
+        url_open.read().decode('utf-8')), delimiter=';')
+    csv_data = [line for line in reader]
+    return csv_data
 
-dict_pessoas = read_data(filename_pessoa_pessoa)
-dict_faccao = read_data(filename_pessoa_faccao)
-dict_ocorrencia = read_data(filename_ocorrencia)
-dict_natureza = read_data(filename_natureza)
-dict_arma = read_data(filename_arma)
-dict_modelo = read_data(filename_veiculo_modelo)
-dict_cor = read_data(filename_veiculo_cor)
+
+# dict_users = {}
+
+dict_pessoas = read_data(csv_online_to_list(filename_pessoa_pessoa))
+dict_faccao = read_data(csv_online_to_list(filename_pessoa_faccao))
+dict_ocorrencia = read_data(csv_online_to_list(filename_ocorrencia_ocorrencia))
+dict_natureza = read_data(csv_online_to_list(filename_ocorrencia_natureza))
+dict_arma = read_data(csv_online_to_list(filename_ocorrencia_arma))
+dict_modelo = read_data(csv_online_to_list(filename_veiculo_modelo))
+dict_cor = read_data(csv_online_to_list(filename_veiculo_cor))
 
 
 def my_import_data():
     tic = timeit.default_timer()
 
-    import_user(filename_user)
-    create_data(filename_pessoa_faccao, Faccao)
-    create_pessoa(filename_pessoa_pessoa)
-    import_foto(filename_pessoa_foto)
-    import_comparsa(filename_pessoa_comparsa)
+    import_user(filename_auth_user)
+    # create_data(filename_pessoa_faccao, Faccao)
+    # create_pessoa(filename_pessoa_pessoa)
+    # import_foto(filename_pessoa_foto)
+    # import_comparsa(filename_pessoa_comparsa)
 
-    # Ocorrencia
-    import_ocorrencia(filename_ocorrencia)
-    create_data(filename_natureza, Natureza)
-    create_data(filename_arma, Arma)
-    create_data(filename_areaupm, AreaUpm)
-    create_data(filename_motivacao, Motivacao)
-    import_infracao(filename_infracao)
-    import_pessoa_ocorrencia(filename_pessoa_ocorrencia)
+    # # Ocorrencia
+    # import_ocorrencia(filename_ocorrencia_ocorrencia)
+    # create_data(filename_ocorrencia_natureza, Natureza)
+    # create_data(filename_ocorrencia_arma, Arma)
+    # create_data(filename_ocorrencia_areaupm, AreaUpm)
+    # create_data(filename_ocorrencia_motivacao, Motivacao)
+    # import_infracao(filename_ocorrencia_infracao)
+    # import_pessoa_ocorrencia(filename_ocorrencia_pessoaocorrencia)
 
-    # Veiculo
-    create_data(filename_veiculo_cor, Cor)
-    create_data(filename_veiculo_modelo, Modelo)
-    import_veiculo(filename_veiculo_veiculo)
+    # # Veiculo
+    # create_data(filename_veiculo_cor, Cor)
+    # create_data(filename_veiculo_modelo, Modelo)
+    # import_veiculo(filename_veiculo_veiculo)
 
     toc = timeit.default_timer()
     return round(toc - tic, 2)
@@ -100,17 +116,6 @@ def create_data(filename, model):
     aux = df.T.apply(dict).tolist()
     data = [model(**item) for item in aux]
     model.objects.bulk_create(data)
-
-
-def csv_online_to_list(url: str) -> list:
-    '''
-    Lê um CSV a partir de uma url.
-    '''
-    url_open = urllib.request.urlopen(url)
-    reader = csv.DictReader(io.StringIO(
-        url_open.read().decode('utf-8')), delimiter=',')
-    csv_data = [line for line in reader]
-    return csv_data
 
 
 def create_pessoa(filename):
@@ -153,23 +158,25 @@ User
 '''
 
 
-def import_user(filename):
-    df = pd.read_csv(filename)
-    aux = df.T.apply(dict).tolist()
-    for item in aux:
-        dict_users[item['id']] = item['username']
+def import_user(filename_auth_user):
+    dict_users = {}
+    items = csv_online_to_list(filename_auth_user)
+    for item in items:
+        dict_users[str(item['id'])] = item['username']
         del item['id']
+        first_name = item['first_name'] if item['first_name'] != 'nan' else ''
+        last_name = item['last_name'] if item['last_name'] != 'nan' else ''
         User.objects.create(
             username=item['username'],
-            first_name=item['first_name'],
-            last_name=item['last_name'],
-            email=item['email'],
+            first_name=first_name,
+            last_name=last_name,
             is_active=item['is_active'],
             is_staff=item['is_staff'],
             is_superuser=item['is_superuser'],
             password=item['password'],
             date_joined=item['date_joined'],
         )
+
 
 '''
 Pessoa
