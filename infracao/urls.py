1674a7cd05fa22fd6e1ecb96360a1df83ddc87c9
rg3915 <rg3915@yahoo.com.br>
2020-07-11 01:42:57 -0300

    Infracao: arrumar urls e views de infracao. Close #125

--- infracao/urls.py
+++ infracao/urls.py
@@ -6,15 +6,15 @@ app_name = 'infracao'
 
 
 infracao_patterns = [
-    path('', v.InfracaoList.as_view(), name="infracoes"),
-    path('add/', v.infracao_create, name="infracao_create"),
-    path('<slug>/update/', v.InfracaoUpdate.as_view(), name="infracao_update"),
+    path('', v.InfracaoList.as_view(), name='infracao_list'),
+    path('add/', v.infracao_create, name='infracao_create'),
+    path('<slug>/update/', v.InfracaoUpdate.as_view(), name='infracao_update'),
 ]
 
 natureza_patterns = [
-    path('', v.NaturezaList.as_view(), name="naturezas"),
-    path('add/', v.natureza_create, name="natureza_create"),
-    path('<slug>/update/', v.NaturezaUpdate.as_view(), name="natureza_update"),
+    path('', v.NaturezaList.as_view(), name='natureza_list'),
+    path('add/', v.natureza_create, name='natureza_create'),
+    path('<slug>/update/', v.NaturezaUpdate.as_view(), name='natureza_update'),
 ]
 
 
