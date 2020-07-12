04156b14c6b7ec514149b84ec0a63ffb85f9775c
rg3915 <rg3915@yahoo.com.br>
2020-07-12 00:58:12 -0300

    Template Infracao completo list. Close #156
    Definir cores para os status. Close #118

--- infracao/admin.py
+++ infracao/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Arma
+from .models import Natureza
+from .models import Infracao
 
-# Register your models here.
+
+@admin.register(Arma)
+class ArmaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', )
+
+    # if not settings.DEBUG:
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Natureza)
+class NaturezaAdmin(admin.ModelAdmin):
+    list_display = ('artigo', 'natureza')
+    search_fields = ('natureza',)
+
+    # if not settings.DEBUG:
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Infracao)
+class InfracaoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'qualificacao', 'arma', 'status')
+    search_fields = ('pessoa__nome',)
+    list_filter = ('qualificacao', 'arma', 'status')
+
+    # if not settings.DEBUG:
+    def has_delete_permission(self, request, obj=None):
+        return False
