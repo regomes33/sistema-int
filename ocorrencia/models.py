8b7240f46eea18b66d14fa431cba22cbd526550a
Regis Santos <rg3915@yahoo.com.br>
2020-10-21 03:07:12 -0300

    Ocorrencia - RAI único. Close #247

--- ocorrencia/models.py
+++ ocorrencia/models.py
@@ -7,7 +7,7 @@ from pessoa.models import Pessoa, Veiculo
 
 
 class Ocorrencia(UuidModel, CreatedBy, TimeStampedModel):
-    rai = models.IntegerField('RAI', null=True, blank=True)
+    rai = models.IntegerField('RAI', unique=True)
     data_do_fato = models.DateField('Data do Fato')
     descricao = models.TextField('descrição', null=True, blank=True)
 
