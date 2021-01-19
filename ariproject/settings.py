b1749878ae646f48ba89207d16e5e3d8ce97b8c3
Regis Santos <rg3915@yahoo.com.br>
2021-01-19 02:43:52 -0300

    Try staticfiles with WhiteNoise

--- ariproject/settings.py
+++ ariproject/settings.py
@@ -129,4 +129,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
